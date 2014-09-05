aa80d9ded6f910249649bb29ddfcd0a31a949319
awesomeharvest <kzm_temp@mac.com>
2014-09-05 18:39:27 +0900

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+First::Application.config.secret_key_base = 'cde9d709712c0dd2701ff0ba694be9cbb96178e054e49f2423071aaeb5654456313157465d82fe98f0c189f749337f0970ac127bde9b2859a69eb2bf08fc75ae'
